ced6959569fe34d36c0f9d5ab36fe88dbd475daf
Anselmo F.De Bom <ansdebom@gmail.com>
2020-11-05 13:50:44 -0300

    Criação das rotas node.js

--- meuApp/routes/index.js
+++ meuApp/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var ContatoController = require("../controllers/ContatoController");
+var HomeController = require("../controllers/HomeController");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+
+
+
+router.get("/contato", ContatoController.index);
+router.get("/home", HomeController.index);
+router.get("/", HomeController.index);
 
 module.exports = router;
+
+
